9d53154615dfa628a77345661d56689cb5a072d9
Nishant Nawarkhede <nishant.nawarkhede@gmail.com>
2015-08-24 18:34:32 +0530

    updated code

--- testapp/urls.py
+++ testapp/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
         namespace='rest_framework')),
+    url(r'^', include('snippets.urls')),
 ]
